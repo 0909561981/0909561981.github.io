4c3cd7dc7b5140110e48d990cc84e197ec4674ad
0909561981 <104056231+0909561981@users.noreply.github.com>
2025-05-31 23:09:50 +0800

    放大縮小問題嘗試修復

--- Game/main.js
+++ Game/main.js
@@ -78,3 +78,21 @@ function draw() {
     // draw()
     game_facade.draw();
 }
+
+// 處理放大縮小問題
+window.onload = () => {
+  document.addEventListener('touchstart', (event) => {
+    if (event.touches.length > 1) {
+       event.preventDefault();
+    }
+  });
+  
+  let lastTouchEnd = 0;
+  document.addEventListener('touchend', (event) => {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+}
